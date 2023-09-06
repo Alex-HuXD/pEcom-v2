fc5a91bc0ddcf49f5820306e784c5fa0054b0835
XinDi Hu <HuXDev@pm.me>
2023-09-05 23:08:36 -0500

    added firebase sign in

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, } from 'react-router-dom'
 
 import HomePage from './routes/homePage/HomePage'
 import Nav from './routes/navBar/NavBar'
+import SignIn from './routes/signin/Signin'
+
 
 
 const Shop = () => {
@@ -19,6 +21,7 @@ function App() {
             <Route path='/' element={<Nav />}>
                 <Route index element={<HomePage />} />
                 <Route path='/shop' element={<Shop />} />
+                <Route path='/sign-in' element={<SignIn />} />
             </Route>
         </Routes>
     )
---
+++ src/utils/firebase/firebase.utils.tsx
@@ -0,0 +1,53 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyAGaJRebUUoWSSOJY_51iAOVmhPdP9F-vk',
+    authDomain: 'react-ecom-fe389.firebaseapp.com',
+    projectId: 'react-ecom-fe389',
+    storageBucket: 'react-ecom-fe389.appspot.com',
+    messagingSenderId: '516543011258',
+    appId: '1:516543011258:web:9f4efed69b46cf054ea583',
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider()
+provider.setCustomParameters({
+    prompt: 'select_account',
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocFromAuth = async userAuth => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef)
+
+    
+
+    // if user doesnt exsit, create user
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth
+        const createAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createAt,
+            })
+        } catch (error) {
+            console.log('something went wrong', error)
+        }
+    }
+
+    return userDocRef
+}
